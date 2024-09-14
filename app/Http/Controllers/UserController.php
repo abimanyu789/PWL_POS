32d11222c3afe34df6f440560e584d18c158f7fa
abimanyu789 <anandaabimanyu7@gmail.com>
2024-09-15 00:31:21 +0700

    js3 praktikum6

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UserModel;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        // Tambah data user dengan Eloquent Model
+        $data = [
+            'nama' => 'Pelanggan Pertama',
+        ];
+        // Update data user dengan username 'customer-1'
+        UserModel::where('username', 'customer-1')->update($data); 
+        // Coba akses model UserModel
+        $user = UserModel::all(); // Ambil semua data dari tabel m_user
+        return view('user', ['data' => $user]);
+    }
+}
