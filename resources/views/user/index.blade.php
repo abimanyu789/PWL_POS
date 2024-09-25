25d371f3ea37e9cea8ddfe3260aec2cb277b50b2
abimanyu789 <anandaabimanyu7@gmail.com>
2024-09-26 01:19:05 +0700

    js5 praktikum4

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,6 +14,22 @@
             @if (session('error'))
                 <div class="alert alert-danger">{{ session('error') }}</div>
             @endif
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="form-group row">
+                        <label class="col-1 control-label col-form-label">Filter: </label>
+                        <div class="col-3">
+                            <select class="form-control" id="level_id" name="level_id" required>
+                                <option value="">- Semua </option>
+                                @foreach ($level as $item)
+                                    <option value="{{ $item->level_id }}">{{ $item->level_nama }}</option>
+                                @endforeach
+                            </select>
+                            <small class="form-text text-muted">Level Pengguna</small>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
                 <thead>
                     <tr>
@@ -26,18 +42,6 @@
                 </thead>
             </table>
         </div>
-        <table class="table table-bordered table-striped table-hover table-sm" id="table_user">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Username</th>
-                    <th>Nama</th>
-                    <th>Level Pengguna</th>
-                    <th>Aksi</th>
-                </tr>
-            </thead>
-        </table>
-    </div>
     </div>
 @endsection
 @push('css')
@@ -51,7 +55,10 @@
                 ajax: {
                     "url": "{{ url('user/list') }}",
                     "dataType": "json",
-                    "type": "POST"
+                    "type": "POST",
+                    "data" : function(d){
+                        d.level_id = $('#level_id').val();
+                    }
                 },
                 columns: [{
                     // nomor urut dari laravel datatable addIndexColumn()
@@ -84,6 +91,11 @@
                     searchable: false
                 }]
             });
+
+            $('#level_id').on('change', function(){
+                dataUser.ajax.reload();
+            })
+
         });
     </script>
 @endpush
