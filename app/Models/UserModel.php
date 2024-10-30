97ef7fa627a53fd85d0166ff4d70c3f7835a8563
abimanyu789 <anandaabimanyu7@gmail.com>
2024-10-30 09:09:57 +0700

    js10 praktikum1

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,9 +5,19 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Foundation\Auth\User as Authenticatable; // Implementasi class Aunthenticatable
+use Tymon\JWTAuth\Contracts\JWTSubject;
 
-class UserModel extends Authenticatable
+class UserModel extends Authenticatable implements JWTSubject
 {
+    public function getJWTIdentifier()
+    {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims()
+    {
+        return [];
+    }
+
     use HasFactory;
 
     protected $table = 'm_user'; // Mendefinisikan nama tabel yang digunakan oleh model ini
--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,10 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        'api' => [
+            'driver' => 'jut',
+            'provider' => 'users'
+        ],
     ],
 
     /*
